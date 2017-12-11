2a4baa4f1e72c0c457e90d946b63e47bb499ee1a
AbdulRahman AlHamali <a.alhamali@saugo360.com>
2017-12-11 09:55:56 +0200

    adding missing files

---
+++ calculate_controllability.py
@@ -0,0 +1,72 @@
+
+
+def calculateControllability(levels, circuitDescription, testability):
+	for level in levels:
+		for lineInd in level:
+			if testability.get(lineInd) is None:
+				testability[lineInd] = {}
+			lineInfo = level[lineInd]
+			control0 = calculateControllabilityToZero(lineInfo, circuitDescription, testability)
+			testability[lineInd]["control0"] = control0
+			control1 = calculateControllabilityToOne(lineInfo, circuitDescription, testability)
+			testability[lineInd]["control1"] = control1
+
+def calculateControllabilityToZero(lineInfo, circuitDescription, testability):
+	gate = lineInfo["leaving"]
+	if gate == None:
+		control0 = 1
+		return control0
+	else:
+		control0 = 0
+		gateType = circuitDescription[2][gate]["type"]
+		if gateType == "nand" or gateType == "or":
+			for line in  circuitDescription[2][gate]["inputs"]:
+				control0 = testability[line]["control0"] + control0
+			control0 = control0 + 1
+			return control0
+		elif gateType == "fanout":
+			line = circuitDescription[2][gate]["inputs"][0]
+			control0 = testability[line]["control0"]
+			return control0
+		elif gateType == "and" or gateType == "nor":
+			line = circuitDescription[2][gate]["inputs"][0]
+			control0 = testability[line]["control0"]
+			for line in  circuitDescription[2][gate]["inputs"]:
+				control0 = min(control0, testability[line]["control0"])
+			control0 = control0 + 1
+			return control0
+		elif gateType == "not":
+			line = circuitDescription[2][gate]["inputs"][0]
+			control0 = testability[line]["control0"] + 1
+			return control0
+
+def calculateControllabilityToOne(lineInfo, circuitDescription, testability):
+
+    gate = lineInfo["leaving"]
+    if gate == None:
+        control1 = 1
+        return control1
+    else:
+        control1 = 0
+        gateType = circuitDescription[2][gate]["type"]
+
+        if gateType == "and" or gateType == "nor":
+            for line in circuitDescription[2][gate]["inputs"]:
+                control1 = testability[line]["control1"] + control1
+            control1 = control1 + 1
+            return control1
+        elif gateType == "fanout":
+            line = circuitDescription[2][gate]["inputs"][0]
+            control1 = testability[line]["control1"]
+            return control1
+        elif gateType == "nand" or gateType == "or":
+            line = circuitDescription[2][gate]["inputs"][0]
+            control1 = testability[line]["control1"]
+            for line in circuitDescription[2][gate]["inputs"]:
+                control1 = min(control1, testability[line]["control1"])
+            control1 = control1 + 1
+            return control1
+        elif gateType == "not":
+            line = circuitDescription[2][gate]["inputs"][0]
+            control1 = testability[line]["control1"] + 1
+            return control1
---
+++ calculate_observability.py
@@ -0,0 +1,42 @@
+def calculateObservability(levels, circuitDescription, testability):
+	for level in reversed(levels):
+		for lineInd in level:
+			lineInfo = level[lineInd]
+			observ = calculateLineObservability(lineInfo, lineInd, circuitDescription, testability)
+			testability[lineInd]["obs"] = observ
+
+def calculateLineObservability(lineInfo, lineInd, circuitDescription, testability):
+    gate = lineInfo["entering"]
+    if gate == None:
+        obs = 0
+        return obs
+    else:
+        gateType = circuitDescription[2][gate]["type"]
+        outputLine = circuitDescription[2][gate]["outputs"]
+        if gateType == "and" or gateType == "nand":
+            obs = testability[outputLine[0]]["obs"]
+            for line in circuitDescription[2][gate]["inputs"]:
+                if lineInd == line:
+                    pass
+                else:
+                    obs = obs + testability[line]["control1"]
+
+            obs = obs + 1
+            return obs
+        elif gateType == "or" or gateType == "nor":
+            obs = testability[outputLine[0]]["obs"]
+            for line in circuitDescription[2][gate]["inputs"]:
+                if lineInd == line:
+                    pass
+                else:
+                    obs = obs + testability[line]["control0"]
+            obs = obs + 1
+            return obs
+        elif gateType == "not":
+            obs = testability[outputLine[0]]["obs"] + 1
+            return obs
+        elif gateType == "fanout":
+            obs = testability[outputLine[0]]["obs"]
+            for line in circuitDescription[2][gate]["outputs"]:
+                obs = min(obs, testability[line]["obs"])
+            return obs
